6510547a54c35e300471573f1b5d314c8068536b
jsimpson <jsimpson@nrel.gov>
2018-05-10 13:48:53 -0600

    Added forward of simulation out to testing and processing of measurements as an array.

--- sample_app/tests/app_rules.py
+++ sample_app/tests/app_rules.py
@@ -1,12 +1,19 @@
 from durable.lang import *
 import argparse
-import dateutil.parser
+import datetime
+import time
 from collections import defaultdict
 import stomp
 import json
 import math
 
+from gridappsd import GOSS
+goss = GOSS()
+goss.connect()
+
 goss_log = 'goss.gridappsd.platform.log'
+responseQueueTopic = '/temp-queue/response-queue'
+
 def run_rules(topic='input',port=5000, run_start = "2017-07-21 12:00:00", run_end = "2017-07-22 12:00:00"):
     #2017-07-21T18:00Z 2017-07-22T18:00Z
     print ("Start data {0} and end date {1}".format(run_start,run_end))
@@ -30,22 +37,39 @@ def run_rules(topic='input',port=5000, run_start = "2017-07-21 12:00:00", run_en
     except:
         print ("Not connected to GOSS - messages will not be logged on the platform")
 
+
     def send_log_msg(msg):
+        print ('Send log')
         logMsg['logMessage'] = msg
+        now = datetime.datetime.now()
+        logMsg['timestamp'] = now.strftime("%Y-%m-%d %H:%M:%S")
+        t_now = datetime.datetime.utcnow()
+        logMsg['timestamp'] = int(time.mktime(t_now.timetuple()) * 1000) + t_now.microsecond
+        print logMsg['timestamp']
         logMsgStr = json.dumps(logMsg)
-        gossConnection.send(body=logMsgStr, destination=goss_log,
-                            headers={'reply-to': "/temp-queue/response-queue"})
+        print gossConnection.send(body=logMsgStr, destination=goss_log,
+                            headers={'reply-to':responseQueueTopic})
+        goss.send(body=logMsgStr, destination=goss_log)
 
     logMsg = {
         # 'id': 401,
         'source': 'rule',
         'processId': 'simple_app.rule',
-        'timestamp': '2018-01-23 00:00:00',
+        'timestamp': '2018-05-09 00:00:00',
         'logLevel': 'INFO',
         'logMessage': 'template msg',
-        'processStatus': 'RUNNING',
-        'username': username,
+        'processStatus': 'ERROR',
+        # 'username': username,
         'storeToDb': True}
+
+    # status_message = {
+    #     "source": os.path.basename(__file__),
+    #     "processId": str(simulation_id),
+    #     "timestamp": int(time.mktime(t_now.timetuple()) * 1000) + t_now.microsecond,
+    #     "processStatus": status,
+    #     "logMessage": str(message),
+    #     "logLevel": log_level,
+    #     "storeToDb": True}
 
 
     testInput = ruleset(topic)
@@ -62,17 +86,53 @@ def run_rules(topic='input',port=5000, run_start = "2017-07-21 12:00:00", run_en
         ## Get start and end from TestConfig
 
         # Check a certain mrid's measurement Voltage P.U.
-        @when_all(m.message.measurement.measurement_mrid == "123a456b-789c-012d-345e-678f901a234b")
-        def node_meas_check2(c):
-            # consequent
-            mag = c.m.message.measurement.magnitude
-            ang = c.m.message.measurement.angle
-            c_temp = complex(mag, ang)
-            volt_pu = (math.sqrt(c_temp.real**2 + c_temp.imag**2) / (base_kv) ) / math.sqrt(3)
-            if volt_pu < .95 or volt_pu > 1.05:
-                print "Voltage out of threshold " + str(volt_pu) + " at " + c.m.message.timestamp
-                send_log_msg("Voltage out of threshold " + str(volt_pu) + " at " + c.m.message.timestamp)
+        #u'PowerTransformer_hvmv_sub_Voltage
+        transformer_voltages = ['b74b5d31-158e-49a7-8f92-3042598cfd66',
+                             '95ca5999-6b99-49cd-b6bb-9468cfd66680',
+                             '6ac54b7c-3eff-4e0c-8a84-53ceedeb5188']
+        transformer_voltages = {u'6ac54b7c-3eff-4e0c-8a84-53ceedeb5188': u'PowerTransformer_hvmv_sub_Voltage_C',
+         u'95ca5999-6b99-49cd-b6bb-9468cfd66680': u'PowerTransformer_hvmv_sub_Voltage_B',
+         u'b74b5d31-158e-49a7-8f92-3042598cfd66': u'PowerTransformer_hvmv_sub_Voltage_A'}
 
+        @when_all(+m.message.measurements)
+        def node_meas_check3(c):
+            # consequent
+            measurements = c.m.message.measurements
+            print ("Magnitude check " + str(len(measurements)) + ' ' + measurements[0]['measurement_mrid'])
+            for measurements_ in c.m.message.measurements:
+                if measurements_['measurement_mrid'] in transformer_voltages.keys():
+                    mag = measurements_['magnitude']
+                    ang = measurements_['angle']
+                    check_voltage(c, measurements_['measurement_mrid'], mag, ang, base_kv)
+
+        # @when_all(+m.message.measurement.magnitude)
+        # def node_meas_check1(c):
+        #     # consequent
+        #     print ("Magnitude check")
+        #     if c.m.message.measurement.measurement_mrid in transformer_voltages:
+        #         check_voltage(c, base_kv)
+        #
+        # @when_all(m.message.measurement.measurement_mrid == "123a456b-789c-012d-345e-678f901a234b")
+        # def node_meas_check2(c):
+        #     # consequent
+        #     check_voltage(c, base_kv)
+
+        def check_voltage(c, mrid, mag, ang, base_kv):
+            c_temp = complex(mag, ang)
+            volt_pu = (math.sqrt(c_temp.real ** 2 + c_temp.imag ** 2) / (base_kv)) / math.sqrt(3)
+            print ("Magnitude check " + str(volt_pu))
+            if volt_pu < .95 or volt_pu > 1.05:
+                print (transformer_voltages[mrid] + " voltage p.u. out of threshold " + str(volt_pu) + " at " + c.m.message.timestamp)
+                send_log_msg(transformer_voltages[mrid] + " voltage p.u. out of threshold " + str(volt_pu) + " at " + c.m.message.timestamp)
+
+        # def check_voltage(c, base_kv):
+        #     mag = c.m.message.measurement.magnitude
+        #     ang = c.m.message.measurement.angle
+        #     c_temp = complex(mag, ang)
+        #     volt_pu = (math.sqrt(c_temp.real ** 2 + c_temp.imag ** 2) / (base_kv)) / math.sqrt(3)
+        #     if volt_pu < .95 or volt_pu > 1.05:
+        #         print "Voltage out of threshold " + str(volt_pu) + " at " + c.m.message.timestamp
+        #         send_log_msg("Voltage out of threshold " + str(volt_pu) + " at " + c.m.message.timestamp)
 
         # A Reverse and a Forward difference is a state change.
         @when_all((m.message.reverse_difference.attribute == 'Switch.open') & (
@@ -116,6 +176,7 @@ def run_rules(topic='input',port=5000, run_start = "2017-07-21 12:00:00", run_en
 
         @when_start
         def start(host):
+            print('Topic', topic)
 
             host.assert_fact(topic, {'mrid': 1, 'time':1})
             host.assert_fact(topic, {'mrid': 1, 'time':2})
@@ -129,26 +190,27 @@ def run_rules(topic='input',port=5000, run_start = "2017-07-21 12:00:00", run_en
                 "simulation_id" : "12ae2345",
                 "message" : {
                     "timestamp" : "2018-01-08T13:27:00.000Z",
-                    "measurement" : {
-                        "measurement_mrid" : "123a456b-789c-012d-345e-678f901a234b",
+                    "measurements" : [{
+                        "measurement_mrid" : "b74b5d31-158e-49a7-8f92-3042598cfd66",
                         "magnitude" : 1960.512425,
                         "angle" : 6912.904192
-                    }
+                    }]
                 }
             }
             meas2 = {
                 "simulation_id" : "12ae2345",
                 "message" : {
                     "timestamp" : "2018-01-08T13:27:00.000Z",
-                    "measurement" : {
-                        "measurement_mrid" : "123a456b-789c-012d-345e-678f901a234b",
+                    "measurements" : [{
+                        "measurement_mrid" : "b74b5d31-158e-49a7-8f92-3042598cfd66",
                         "magnitude" : 4154.196028,
                         "angle" : -4422.093355
-                    }
+                    }]
                 }
             }
             host.post(topic, meas1)
             host.post(topic, meas2)
+
             # host.post(topic, {"simulation_id": "12ae2345", "message": {"timestamp": "2018-01-08T13:27:00.000Z",
             #                                                      "difference_mrid": "123a456b-789c-012d-345e-678f901a235c",
             #                                                      "reverse_differences": [
---
+++ sample_app/tests/goss_forward_sim.py
@@ -0,0 +1,168 @@
+import requests
+import logging
+import os
+import signal
+import sys
+import time
+import traceback
+
+import json
+import yaml
+from gridappsd import GOSS
+
+logger_name = "goss-forward"
+user_home = os.path.expanduser("~")
+logger_location = os.path.join(user_home, "var/log/" + logger_name + ".log")
+
+if not os.path.exists(os.path.dirname(logger_location)):
+    os.makedirs(os.path.dirname(logger_location))
+
+read_topic = 'goss.gridappsd.simulation.output.'
+
+goss_connection = None
+is_initialized = False
+# Number
+simulation_id = None
+
+# This is currently what gridlabd will use for its object name.
+# TODO change to use concat of sim id and sim name.
+simulation_name = None
+
+hdlr = logging.FileHandler(logger_location)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+
+logger = logging.getLogger(logger_name)
+logger.addHandler(hdlr)
+logger.setLevel(logging.DEBUG)
+
+
+# Created in the bottom of the script before the instantiation of the
+# GossListener.
+
+
+class GOSSListener(object):
+    def __init__(self, t0):
+        self.t0 = t0
+        self.expected = "expected_result_series.json"
+        self.raw_str_file = "expected_str.json"
+        self.raw_str_data = 'None'
+        self.data = {"expected_outputs": {}}
+        self.msgCount = 0
+
+    def on_message(self, headers, msg):
+
+        try:
+            self.t0 += 1
+            logger.debug('received message ' + str(msg[:200]))
+
+            json_msg = yaml.safe_load(str(msg))
+
+            # print str(msg)
+            if 'output' not in json_msg or json_msg['output'] == None:
+                print 'Output is none'
+            output = yaml.safe_load(json_msg['output'])
+            # Ignore null output data. (Assumes initializing)
+            if output is None:
+                return
+
+            temp_meas = output['message']['measurements']
+            len2 = len(output['message']['measurements'])
+            start = 0
+            chunk_size = 500
+            for end in range(chunk_size, len2 - 1, chunk_size):
+                output['message']['measurements'] = temp_meas[start:end]
+                r = requests.post("http://localhost:5000/input/events", json=output)
+                start = end
+            # print(start, len2 - 1)
+            output['message']['measurements'] = temp_meas[start: len2 - 1]
+            r = requests.post("http://localhost:5000/input/events", json=output)
+
+            print ('Forward ' + str(output)[:500])
+            print r.status_code
+            print r.headers
+
+            self.raw_str_data = str(msg)
+            self.save()
+            # self.data["expected_outputs"][str(self.msgCount)] = output
+            # self.msgCount+=1
+
+            print("the output is: {}".format(str(output)[:500]))
+
+        except Exception as e:
+            traceback.print_exc(file=sys.stdout)
+            logger.error(type(e))
+            logger.error(e.args)
+            logger.error('Error in command ' + str(e))
+
+    def on_error(self, headers, message):
+        logger.error('Error in goss listener ' + str(message))
+
+    def on_disconnected(self):
+        logger.error('Disconnected')
+
+    def save(self):
+        with open(self.expected, 'w') as outfile:
+            json.dump(self.data, outfile, sort_keys = True, indent = 2)
+
+        with open(self.raw_str_file, 'w') as outfile:
+            json.dump(self.raw_str_data, outfile, sort_keys=True, indent=2)
+
+goss_listener = GOSSListener(2)
+
+def _keep_alive():
+    while 1:
+        time.sleep(0.1)
+
+def signal_handler(signal, frame):
+    print('You pressed Ctrl+C!')
+    goss_listener.save()
+    sys.exit(0)
+
+def _register_with_goss(username, password, gossServer='localhost',
+                      stompPort='61613', read_topic=read_topic):
+    """Register with the GOSS server broker and return.
+
+    Function arguments:
+        gossServer -- Type: string. Description: The ip location
+        for the GOSS server. It must not be an empty string.
+            Default: 'localhost'.
+        stompPort -- Type: string. Description: The port for Stomp
+        protocol for the GOSS server. It must not be an empty string.
+            Default: '61613'.
+        username -- Type: string. Description: User name for GOSS connection.
+        password -- Type: string. Description: Password for GOSS connection.
+
+    Function returns:
+        None.
+    Function exceptions:
+        RuntimeError()
+    """
+    goss = GOSS()
+    goss.connect()
+    print ('GOSS connected ' + str(goss.connected))
+    goss.subscribe(read_topic, goss_listener)
+
+def forward_sim_ouput(simID):
+    _register_with_goss('system','manager',gossServer='127.0.0.1',stompPort='61613',read_topic=read_topic+simID)
+    signal.signal(signal.SIGINT, signal_handler)
+    _keep_alive()
+
+if __name__ == "__main__":
+    import argparse
+
+#    parser = argparse.ArgumentParser(version=__version__)
+#    parser.add_argument("simulationId",
+#                        help="Simulation id to use for responses on the message bus.")
+
+    logger.debug("Waiting for ")
+
+    # Connect and listen to the message bus for content.
+    # The port should be cast to string because that makes the opening socket easier.
+    _register_with_goss('system','manager',gossServer='127.0.0.1',stompPort='61613')
+    #_register_with_goss(opts.user, opts.password, opts.address, str(opts.port))
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Sleep until notified of new data.
+    _keep_alive()
